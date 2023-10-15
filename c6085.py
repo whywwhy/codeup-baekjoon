eb3b886912a6deb6942970976ca936a4bb2a474e
je355 <120763503+je355@users.noreply.github.com>
2023-10-15 23:30:08 +0900

    code up

---
+++ c6085.py
@@ -0,0 +1,7 @@
+w, h, b = input().split(' ')
+w = int(w)
+h = int(h)
+b = int(b) 
+e = (w*h*b)/(8*1024*1024)
+print(format(e,".2f"), end=' ')
+print('MB')
---
+++ c6086.py
@@ -0,0 +1,10 @@
+n = int(input())
+i = 0
+sum = 0
+while True:
+    i+=1 
+    sum+=i 
+    if sum>=n:
+        break
+print(sum)
+
