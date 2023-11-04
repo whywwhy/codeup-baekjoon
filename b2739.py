2afe10d7a2542689f217ab78330dce5a158b71aa
je355 <120763503+je355@users.noreply.github.com>
2023-11-04 21:25:12 +0900

    baekjoon

---
+++ b2739.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+print(str(n)+" * 1 = "+str(n*1))
+print(str(n)+" * 2 = "+str(n*2))
+print(str(n)+" * 3 = "+str(n*3))
+print(str(n)+" * 4 = "+str(n*4))
+print(str(n)+" * 5 = "+str(n*5))
+print(str(n)+" * 6 = "+str(n*6))
+print(str(n)+" * 7 = "+str(n*7))
+print(str(n)+" * 8 = "+str(n*8))
+print(str(n)+" * 9 = "+str(n*9))
