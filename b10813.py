3598ffa0e6faffbab9a5e2de9700b115159b8097
je355 <120763503+je355@users.noreply.github.com>
2023-11-20 22:58:54 +0900

    baekjoon

---
+++ b10813.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+b = [i for i in range(1, n+1)]
+
+for i in range(m):
+    i, j = map(int, input().split())
+    b[i-1], b[j-1] = b[j-1], b[i-1]
+
+for i in range(n):
+    print(b[i], end= ' ')
